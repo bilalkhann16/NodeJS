16cac53ae5cc3d0ed2cdd88acc37e6ba2c06ac06
Bilal K <lalbikhan2014@gmail.com>
2022-03-21 18:38:45 +0500

    http node module

--- http-module.js
+++ http-module.js
@@ -5,12 +5,10 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     if (req.url === '/') {
         res.end('Hello World');
-    }
-
+    };
     if (req.url === '/about') {
         res.end('About page response');
-    }
-    
+    };
     res.end(
         `<h1>Page you're trying to find doesn't exist. </h1>`
     )
