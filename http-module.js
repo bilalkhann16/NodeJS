d9065824bfd3f17f569150f77efa123333c25835
Bilal K <lalbikhan2014@gmail.com>
2022-03-21 18:37:24 +0500

    http node module

--- http-module.js
+++ http-module.js
@@ -6,10 +6,14 @@ const server = http.createServer((req, res) => {
     if (req.url === '/') {
         res.end('Hello World');
     }
+
+    if (req.url === '/about') {
+        res.end('About page response');
+    }
     
-    console.log(req)
-    res.write('Welcome, This is Bilal K.');
-    res.end()
+    res.end(
+        `<h1>Page you're trying to find doesn't exist. </h1>`
+    )
 })
 
 server.listen(8888)
