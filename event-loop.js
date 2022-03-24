c162bc7e465c385adae7ab99a2fd5f06b3ea2e5a
Bilal K <lalbikhan2014@gmail.com>
2022-03-24 12:26:43 +0500

    server event loop

--- event-loop.js
+++ event-loop.js
@@ -1,7 +1,24 @@
-// started operating system proces.
-console.log('First, Hello World');
-setTimeout(() => {
-    console.log('second');
-},0);
+// // started operating system proces.
+// console.log('First, Hello World');
+// setTimeout(() => {
+//     console.log('second');
+// },0);
 
-console.log('third');
+// console.log('third');
+
+//new example (setTimeout vs setInterval)
+// setInterval(() =>{
+//     console.log('Hello');
+// },2000);
+// console.log('I will run first.')
+
+//https server example
+const http = require('http');
+const server = http.createServer((req,res) => {
+    console.log('request event');
+    res.end('Hello World');
+});
+
+server.listen(8888,() => {
+    console.log('server is running on port 8000');
+});
